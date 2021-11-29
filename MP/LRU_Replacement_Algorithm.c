2c18089181352d4306cdbac1f5476419eff29dca
NeemeeshK <nimish.khanzode@gmail.com>
2021-11-29 20:03:59 +0530

    Neemeesh

--- MP/LRU_Replacement_Algorithm.c
+++ MP/LRU_Replacement_Algorithm.c
@@ -1,13 +1,72 @@
 #include <stdio.h>
-
 int main()
 {
-    int arr[]={7,0,1,2,0,3,0,4,2,3,0,3,2},i=4;
+    int arr[]={7,0,7,2,0,3,0,4,2,3,0,3,2},i=0;
     int s0=-1,s1=-1,s2=-1,s3=-1;
-    int miss=4,hit=0;
+    int miss=0,hit=0;
     for(i;i<13;i++){
         printf("%d\t%d\t%d\t%d\n",s0,s1,s2,s3);
-        if(arr[i]==s3){
+        if(s3==-1){
+            s3=arr[i];
+            miss++;
+            continue;
+        }
+        else if(s2==-1){
+            if(s3==arr[i]){
+                hit++;
+                continue;
+            }
+            s2=s3;
+            s3=arr[i];
+            miss++;
+            continue;
+        }
+        else if(s1==-1){
+            if(s3==arr[i]){
+                hit++;
+                continue;
+            }
+            if(s2==arr[i]){
+                hit++;
+                int temp=s3;
+                s3=s2;
+                s2=temp;
+                continue;
+            }
+            s1=s2;
+            s2=s3;
+            s3=arr[i];
+            miss++;
+            continue;
+        }
+        else if(s0==-1){
+            if(s3==arr[i]){
+                hit++;
+                continue;
+            }
+            if(s2==arr[i]){
+                hit++;
+                int temp=s3;
+                s3=s2;
+                s2=temp;
+                continue;
+            }
+            if(s1==arr[i]){
+                hit++;
+                int temp=s3;
+                s3=s1;
+                s1=s2;
+                s2=temp;
+                continue;
+            }
+            s0=s1;
+            s1=s2;
+            s2=s3;
+            s3=arr[i];
+            miss++;
+            continue;
+        }
+        else if(arr[i]==s3){
             hit++;
             continue;
         }
